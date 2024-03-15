4f7c32b78c7866eee007c42fccdddbec3728625a
enrique <enrique@nevermined.io>
2024-03-15 13:01:14 +0100

    fix: pages order

--- build-docs.sh
+++ build-docs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 lazydocs \
-    --output-path="./docs/docstrings" \
+    --output-path="./docs" \
     --overview-file="README.md" \
     --src-base-url="https://github.com/nevermined-io/payments-py/blob/main/" \
     payments_py
