3a0d0da7a1b838dc1f058a3a1c9b0dd6a1a313c8
enrique <enrique@nevermined.io>
2024-10-15 11:20:46 +0200

    feat: update test

--- tests/protocol_test.py
+++ tests/protocol_test.py
@@ -10,9 +10,6 @@ from payments_py.data_models import AgentExecutionStatus, CreateAssetResultDto, 
 response_event = asyncio.Event()
 room_joined_event = asyncio.Event()
 global response_data
-global subscription
-global agent
-
 response_data = None
 
 # Set environment variables for the test
@@ -77,9 +74,7 @@ async def on_join_rooms(data):
     room_joined_event.set()
 
 @pytest.mark.asyncio(loop_scope="session")
-async def test_AIQueryApi_publish_agent_and_buy(ai_query_api_build_fixture, ai_query_api_subscriber_fixture):
-    global subscription
-    global agent
+async def test_AIQueryApi_create_task_in_subscription_purchased(ai_query_api_build_fixture, ai_query_api_subscriber_fixture):
     builder = ai_query_api_build_fixture
     subscriber = ai_query_api_subscriber_fixture
 
@@ -114,13 +109,6 @@ async def test_AIQueryApi_publish_agent_and_buy(ai_query_api_build_fixture, ai_q
     order_response = subscriber.order_subscription(subscription_did=subscription.did)
     assert isinstance(order_response, OrderSubscriptionResultDto)
     print('Subscription ordered:', order_response.success)
-
-@pytest.mark.asyncio(loop_scope="session")
-async def test_AIQueryApi_create_task(ai_query_api_build_fixture, ai_query_api_subscriber_fixture):
-    global subscription
-    global agent
-    builder = ai_query_api_build_fixture
-    subscriber = ai_query_api_subscriber_fixture
 
     subscription_task = asyncio.create_task(builder.ai_protocol.subscribe(eventsReceived))
 
